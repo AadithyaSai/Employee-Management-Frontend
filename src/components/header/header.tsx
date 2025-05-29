baa11008a71cc7da862e3fc3e41aeadd78a7abf3
Aadithya Sai <aadithyas0610@gmail.com>
2025-05-29 09:31:10 +0530

    fixed signout button

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,6 +7,24 @@ const Header = () => {
   const [, setLoginToken] = useLocalStorage("isLoggedIn");
   const navigate = useNavigate();
 
+  const logoutIcon = (
+    <svg
+      fill="none"
+      height="24"
+      viewBox="0 0 24 24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17 16L21 12M21 12L17 8M21 12L7 12M13 16V17C13 18.6569 11.6569 20 10 20H6C4.34315 20 3 18.6569 3 17V7C3 5.34315 4.34315 4 6 4H10C11.6569 4 13 5.34315 13 7V8"
+        stroke="#fff"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      />
+    </svg>
+  );
+
   const handleSignout = () => {
     setLoginToken("false");
     navigate("/login");
@@ -24,7 +42,8 @@ const Header = () => {
           type="button"
           onClick={handleSignout}
         >
-          Sign Out
+          <div className="logout-icon">{logoutIcon}</div>
+          <p className="signout-text">Sign Out</p>
         </button>
       </div>
     </header>
